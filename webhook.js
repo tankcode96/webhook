dd1edf9779956099d64c411596dc816b4f5b846f
kunming100 <kim_tankunming@outlook.com>
2022-08-04 16:24:47 +0800

    feat: 修改测试代码

--- webhook.js
+++ webhook.js
@@ -31,6 +31,7 @@ const server = http.createServer(function (req, res) {
       res.end(JSON.stringify({ ok: true }));
       // 开始部署
       if (event === "push") {
+        console.log(chunk);
         const payload = JSON.parse(encodeURIComponent(chunk));
         console.log("end: payload", payload);
         const child = spawn("sh", [`./${payload.repository.name}.sh`]);
