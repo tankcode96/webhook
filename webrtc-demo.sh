c52f4d2d8d3d8374d4192ca3b890c51b7c11a52e
kunming100 <kim_tankunming@outlook.com>
2022-08-05 22:45:06 +0800

    feat: 修改脚本

--- webrtc-demo.sh
+++ webrtc-demo.sh
@@ -28,15 +28,16 @@ echo "开始构建新镜像"
 docker build -t webrtc-demo:1.0 .
 
 echo "启动新容器"
-docker container run \
-  --name webrtc-demo-container \
-  -p 80:80 -p 443:443 \
+# docker container run \
+#   --name webrtc-demo-container \
+#   -p 80:80 -p 443:443 \
   # -v /usr/local/nginx/logs:/var/log/nginx \
   # -v /usr/local/nginx/html:/usr/share/nginx/html \
   # -v /usr/local/nginx/conf/nginx.conf:/etc/nginx/nginx.conf \
   # -v /usr/local/nginx/conf.d:/etc/nginx/conf.d \
-  -v /usr/local/nginx/ssl:/etc/nginx/ssl/ \
-  -d webrtc-demo:1.0
+  # -v /usr/local/nginx/ssl:/etc/nginx/ssl/ \
+  # -d webrtc-demo:1.0
+docker container run --name webrtc-demo-container -p 80:80 -p 443:443 -v /usr/local/nginx/ssl:/etc/nginx/ssl/ -d webrtc-demo:1.0
 # docker container run -it -p 80:80 -p 443:443 --name webrtc-demo-container -d webrtc-demo:1.0 /bin/bash
 # docker container run -p 80:80 -p 443:443 --name webrtc-demo-container --rm -d webrtc-demo:1.0
 
