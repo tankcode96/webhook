fef28d4a07ac1f15fda443c3b4d0fa16932643a8
kunming100 <kim_tankunming@outlook.com>
2022-08-05 22:42:35 +0800

    feat: 修改脚本

--- webrtc-demo.sh
+++ webrtc-demo.sh
@@ -17,7 +17,7 @@ npm install
 echo "开始编译项目"
 npm run build
 
-echo "停止旧容器并删除旧容器"
+echo "停止并删除旧容器"
 docker stop webrtc-demo-container
 docker rm webrtc-demo-container
 
@@ -28,7 +28,15 @@ echo "开始构建新镜像"
 docker build -t webrtc-demo:1.0 .
 
 echo "启动新容器"
-docker container run -p 80:80 -p 443:443 --name webrtc-demo-container -d webrtc-demo:1.0
+docker container run \
+  --name webrtc-demo-container \
+  -p 80:80 -p 443:443 \
+  # -v /usr/local/nginx/logs:/var/log/nginx \
+  # -v /usr/local/nginx/html:/usr/share/nginx/html \
+  # -v /usr/local/nginx/conf/nginx.conf:/etc/nginx/nginx.conf \
+  # -v /usr/local/nginx/conf.d:/etc/nginx/conf.d \
+  -v /usr/local/nginx/ssl:/etc/nginx/ssl/ \
+  -d webrtc-demo:1.0
 # docker container run -it -p 80:80 -p 443:443 --name webrtc-demo-container -d webrtc-demo:1.0 /bin/bash
 # docker container run -p 80:80 -p 443:443 --name webrtc-demo-container --rm -d webrtc-demo:1.0
 
